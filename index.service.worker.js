0b656deae51f9bca8a540b5d94f5e3be8b962b34
JordanFH <89503292+JordanFH@users.noreply.github.com>
2022-09-24 23:23:12 -0500

    Add files via upload

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1664062257|8833013362";
+const CACHE_VERSION = "1664069476|1359529339";
 const CACHE_PREFIX = "Platformer 2D-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
